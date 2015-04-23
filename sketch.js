c1748a9b2a94435cfb72dabd610533d38516f7b9
Rachana Oza <rso233@nyu.edu>
2015-04-22 21:22:45 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+/* THIS IS THE OLD CODE
 var ball = [];
 var noOfBalls = 5;
 
@@ -30,4 +31,30 @@ function Shape(ranColor){
   		fill(ranColor);
 	    ellipse(this.x, 10, this.diameter, this.diameter);
     }
+}
+*/
+
+
+
+var movers = [];
+
+function setup() {
+  createCanvas(640, 360);
+  for (var i = 0; i < 5; i++) {
+    movers[i] = new Mover(random(1, 5), random(0,width), 0, color(random(0,255),random(0,255),random(0,255)), random(10,60));
+  }
+}
+
+function draw() {
+  background(0);
+  
+  for (var i = 0; i < movers.length; i++) {
+    var wind = createVector(0.01, 0);
+    var gravity = createVector(0, 0.1);
+    movers[i].applyForce(wind);
+    movers[i].applyForce(gravity);
+    movers[i].update();
+    movers[i].display();
+    movers[i].checkEdges();
+  }
 }
